e441fd65b0d30aea013dcc343683ae38d829e9c0
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-07-15 22:21:41 +0530

    NEW

--- src/app/services/recruitment-solution/page.js
+++ src/app/services/recruitment-solution/page.js
@@ -1,15 +1,19 @@
 import Mission from '@/components/servicepage/RecruitmentSolution/Mission';
-import ExpertIt from '@/components/servicepage/RecruitmentSolution/ExpertIt';
+import ExpertIt from '@/components/servicepage/RecruitmentSolution/Expert';
 import Itrecruitment from '@/components/servicepage/RecruitmentSolution/Itrecruitment';
-import FAQs from '@/components/servicepage/RecruitmentSolution/FAQs';
+import FAQs from '@/components/servicepage/RecruitmentSolution/Faq';
 
 export default function Home() {
     return (
-        <div className="max-w-[1440px] mx-auto">
-            <ExpertIt />
-            <Mission />
-            <Itrecruitment />
-            <FAQs />
-        </div>
+        <>
+            <div>
+                <ExpertIt />
+                <Mission />
+                <Itrecruitment />
+                <FAQs />
+            </div>
+
+
+        </>
     );
 }
