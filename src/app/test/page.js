f12bbaf1414417a30dea5947df20359c5b5e478d
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-07-15 11:08:24 +0530

    new

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,3 +1,4 @@
+
 import RecruitmentProcess from '@/components/tests/Test'
 import StreamlinedITSolutions from '@/components/tests/Test2'
 import React from 'react'
@@ -5,7 +6,7 @@ import React from 'react'
 const page = () => {
     return (
         <div>
-            <StreamlinedITSolutions />
+            <RecruitmentProcess />
         </div>
     )
 }
