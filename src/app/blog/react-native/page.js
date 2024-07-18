6923c98cc4138d1f1e8001439b454cf1f252e20d
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-07-18 12:37:49 +0530

    bloger

--- src/app/blog/react-native/page.js
+++ src/app/blog/react-native/page.js
@@ -1,12 +1,14 @@
 import ReactNative from '@/components/bloginner/Reactnative'
 import React from 'react'
 import NativeLayout from './layout'
+import BlogContact from '@/components/bloginner/BlogContact'
 
 const page = () => {
     return (
         <NativeLayout>
             <div>
                 <ReactNative />
+                <BlogContact />
             </div>
         </NativeLayout>
     )
